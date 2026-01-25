692af2c491190c85cbca1a985881af94022c8624
codewithjaker <mohamadjakerhossain86@gmail.com>
2026-01-25 22:02:02 +0600

    add configuration file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: { unoptimized: true },
+  output: "export",
+  trailingSlash: true,
+  generateEtags: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
